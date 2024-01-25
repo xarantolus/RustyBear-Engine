73013882ae96a9f857a67f2ae8f84585bbfa1ef6
xarantolus <xarantolus@protonmail.com>
2024-01-25 19:42:06 +0100

    Parse ldtk features

--- src/entity/entities.rs
+++ src/entity/entities.rs
@@ -1,5 +1,6 @@
 use glam::{Vec3, Vec4};
 use hashbrown::HashMap;
+use micro_ldtk::ldtk::{LdtkFromBytes, ParseError, Project};
 
 use crate::{
     assets::{assets::Ptr, texture::Texture2D},
@@ -8,6 +9,7 @@ use crate::{
 
 //A collection of entities that represents a set of worlds.
 pub struct Worlds {
+    // World: speichert entities
     worlds: HashMap<Guid, hecs::World>,
     generator: GuidGenerator,
     current_world: Option<Guid>,
@@ -52,5 +54,46 @@ impl Worlds {
 
     pub fn start_world(&mut self, guid: Guid) {
         self.current_world = Some(guid);
+    }
+
+    pub fn from_file(json: &[u8]) -> Result<Self, ParseError> {
+        let mut worlds = Worlds::new();
+        let project = Project::from_bytes(json)?;
+
+        for level in project.worlds {
+            let mut world = hecs::World::new();
+
+            assert!(level.levels.len() == 1, "Only one level per world is supported");
+
+            for level in level.levels {
+                assert!(level.neighbours.is_empty(), "Neighbours are not supported");
+                assert!(
+                    level.layer_instances.is_some(),
+                    "The LDTk option \"Save levels separately\" is not supported."
+                );
+
+                let layers = level.layer_instances.unwrap();
+
+                // layer_instances is in display order with the top layer being first,
+                // all other layers get assigned a lower value
+
+                let z_coord_offset = 0.99 / layers.len() as f32;
+                let mut z_coord = 1.0;
+                for layer in layers {
+                    for tile in layer.grid_tiles {
+                        world.spawn((
+                            Ptr::<Texture2D>::new(Guid::new(tile.t as u64)),
+                            Vec3::new(tile.px[0] as f32, tile.px[1] as f32, z_coord),
+                        ));
+                    }
+
+                    z_coord -= z_coord_offset;
+                }
+            }
+
+            worlds.add_world(world);
+        }
+
+        Ok(worlds)
     }
 }
